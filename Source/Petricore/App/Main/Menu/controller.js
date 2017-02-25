439c27e7d0fb48c07cd70e5e568ea7805cbb6bf1
PSLCOL\jmoreno <jmoreno@psl.com.co>
2017-02-25 16:04:45 -0500

    MUD-281 styling menu and createUpdate view

--- Source/Petricore/App/Main/Menu/controller.js
+++ Source/Petricore/App/Main/Menu/controller.js
@@ -8,9 +8,56 @@ function MainController($scope, $rootScope, $log, $location, $state, sessionStor
     $scope.navigateTo = function (route) {
         $scope.actualRoute = route;
         $state.go(route);
+        $scope.toggle();
     };
-
+    
+    $scope.toggle = function () {
+        angular.element(document.querySelector('#wrapper')).toggleClass('toggled');
+    };
+	
     var init = function () {
+        $scope.menuImg = [
+            {
+                name: 'file', 
+                im: '/../../Assets/img/drilling_view_1.svg'
+            },
+            {
+                name: 'config',
+                im: './Assets/img/config.svg'
+            },
+            {
+                name: 'well',
+                im: '../../../Assets/img/well.svg'
+            },
+            {
+                name: 'signals',
+                im: '../../../Assets/img/signals.svg'
+            },
+            {
+                name: 'alarms',
+                im: '../../../Assets/img/alarms.svg'
+            },
+            {
+                name: 'drilling',
+                im: '../../../Assets/img/drilling_view.svg'
+            },
+            {
+                name: 'data',
+                im: '../../../Assets/img/data.svg'
+            },
+            {
+                name: 'wits',
+                im: '../../../Assets/img/wits.svg'
+            },
+            {
+                name: 'utility',
+                im: '../../../Assets/img/utility.svg'
+            },
+            {
+                name: 'admin',
+                im: '../../../Assets/img/admin.svg'
+            }
+        ];
         $scope.menuOptions = sessionStorageService.getRoleMenu();
         $log.log('$scope.menuOptions', $scope.menuOptions);
         $log.info('Controller for Menu  function loaded');
@@ -26,6 +73,7 @@ function MainController($scope, $rootScope, $log, $location, $state, sessionStor
             fromState: fromState
         });
     });
+	
 }
 
 MainController.$inject = ['$scope', '$rootScope', '$log', '$location', '$state', 'sessionStorageService'];
